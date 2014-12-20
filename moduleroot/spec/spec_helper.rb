92a5fe6be1d5764898689ad66e702df699adb1e5
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-12-20 08:57:52 +0100

    Use rspec-puppet-facts

--- moduleroot/spec/spec_helper.rb
+++ moduleroot/spec/spec_helper.rb
@@ -6,6 +6,9 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 require '<%= r %>'
 <% end -%>
 
+require 'rspec-puppet-facts'
+include RspecPuppetFacts
+
 # Workaround for no method in rspec-puppet to pass undef through :params
 class Undef
   def inspect; 'undef'; end
