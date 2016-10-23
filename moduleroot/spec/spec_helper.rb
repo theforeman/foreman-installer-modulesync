1338a4694d2f8830afb5d9903bcc3a239c78158a
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2016-10-23 13:25:05 +0200

    Add custom facts
    This uses the rspec-puppet-facts 1.7 feature to add custom facts. Since
    we often override this in our specs this can greatly reduce duplication
    in our specs.

--- moduleroot/spec/spec_helper.rb
+++ moduleroot/spec/spec_helper.rb
@@ -9,6 +9,10 @@ require '<%= r %>'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
+                                                    # Original fact sources:
+add_custom_fact :concat_basedir, '/tmp'             # puppetlabs-concat
+add_custom_fact :puppetversion, Puppet.version      # Facter, but excluded from rspec-puppet-facts
+
 # Workaround for no method in rspec-puppet to pass undef through :params
 class Undef
   def inspect; 'undef'; end
